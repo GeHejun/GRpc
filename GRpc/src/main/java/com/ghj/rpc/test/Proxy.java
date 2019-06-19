78a0510515ef5cd3437c1a1c381ce866e0bb31dc
gehj <gehj@ziroom.com>
2019-06-19 20:53:13 +0800

    more

---
+++ GRpc/src/main/java/com/ghj/rpc/test/Proxy.java
@@ -0,0 +1,14 @@
+package com.ghj.rpc.test;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class Proxy implements InvocationHandler {
+
+
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        return null;
+    }
+}
